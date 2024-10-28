069b0b7e8fe12f6c3e1695ace073120293d74989
Syed Owais Ahmed <owais@fyxify.com>
2024-10-28 15:03:52 +0500

    spring data rest props example

--- src/main/java/com/fyxify/practice/dao/BtuOfFurnaceRepository.java
+++ src/main/java/com/fyxify/practice/dao/BtuOfFurnaceRepository.java
@@ -2,6 +2,8 @@ package com.fyxify.practice.dao;
 
 import com.fyxify.practice.entity.BtuOfFurnace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "btu-of-furnaces")
 public interface BtuOfFurnaceRepository extends JpaRepository<BtuOfFurnace, Long> {
 }
